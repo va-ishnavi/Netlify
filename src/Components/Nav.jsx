d3e2c6c8bf98ce32e91ac7ef9c8ff6e9f261fd47
Vaishnvai Gupta <vaishnavivaishnavigupta2204@gmail.com>
2024-02-28 19:33:42 +0530

    now

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import './Nav.css';
 import logo from './../asset/v.png';
 import {Link} from 'react-scroll';
-//import menu from '
 
 const Nav = () => {
   return (
@@ -11,22 +10,12 @@ const Nav = () => {
        <img src={logo} alt='logo' className='Logo' />
           <div className="desktopmenu "> 
                <Link activeClass='active'  to='intro' spy={true} smooth={true} offset={-100} duration={500} className='MenuListItem '>Home</Link>
-               <Link activeClass='active'  to='about' spy={true} smooth={true} offset={-100} duration={500} className='MenuListItem'>About</Link>
-               <Link activeClass='active'  to='skills' spy={true} smooth={true} offset={-100} duration={500} className='MenuListItem'>Skills</Link>
-               <Link activeClass='active'  to='projects' spy={true} smooth={true} offset={-100} duration={500} className='MenuListItem'>Projects</Link>
+               <Link className='MenuListItem'>About</Link>
+               <Link className='MenuListItem'>Skills</Link>
+               <Link className='MenuListItem'>Projects</Link>
           </div>
-          {/* <div className='desktopMenubtn' onClick={()=> */}
-          {/* document.getElementById(contact).scrollIntoView(behavior:"smooth")}> */}
+          <div className=''></div>
           <button className='Button '>contact me</button>
-          {/* </div> */}
-
-          {/* <img src={menu} alt='Menu' className='MobileMenu' /> */}
-          <div className="navmenu "> 
-               <Link activeClass='active'  to='intro' spy={true} smooth={true} offset={-100} duration={500} className='MenuListItem '>Home</Link>
-               <Link activeClass='active'  to='about' spy={true} smooth={true} offset={-100} duration={500} className='MenuListItem'>About</Link>
-               <Link activeClass='active'  to='skills' spy={true} smooth={true} offset={-100} duration={500} className='MenuListItem'>Skills</Link>
-               <Link activeClass='active'  to='projects' spy={true} smooth={true} offset={-100} duration={500} className='MenuListItem'>Projects</Link>
-          </div>
         </div>
     
   )
