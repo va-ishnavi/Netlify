86785b63eeda083cf0235bf47222ea06d5feb2be
Vaishnvai Gupta <vaishnavivaishnavigupta2204@gmail.com>
2024-06-26 09:00:54 +0530

    yes

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -26,17 +26,16 @@ const Contact = () => {
         <input type="text" className='Name' placeholder='Your Name' name='your_name'/>
         <input type="email" className='email' placeholder='Your Email' name='your_email'/>
         <textarea className='msg' name='message' rows="5" placeholder='Your Message'></textarea>
-        {/* <a className='sendBtn' href='https://vaishnavi-resume.netlify.app/'> */}
         <button type='submit' value='send' className='submitBtn'>Send message</button>
-        {/* </a> */}
+       
         <div className='links'>
-        <a href='https://www.linkedin.com/in/vaishnavi-gupta-09-june/' target='_blank'>
+        <a href='https://www.linkedin.com/in/vaishnavi-gupta-09-june/'>
         <i  title='Linkedin Link'class="fa-brands fa-linkedin icons"/>
         </a>
-        <a href='https://github.com/va-ishnavi' target='_blank'>
+        <a href='https://github.com/va-ishnavi'>
         <i title='GitHub Link'class="fa-brands fa-github icons"/>
         </a>
-        <a href='mailto:vaishnavivaishnavigupta2204@gmail.com' target='_blank'>
+        <a href='mailto:vaishnavivaishnavigupta2204@gmail.com'>
         <i title='Gmail Link'class="fa-regular fa-envelope icons"/>
         </a>
         </div>
